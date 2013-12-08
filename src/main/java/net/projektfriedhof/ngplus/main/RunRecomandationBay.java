84115ea3742682c2cb888d65407da4c131457296
habales <git@habales.de>
2013-12-08 11:10:53 +0100

    logger configured

--- src/main/java/net/projektfriedhof/ngplus/main/RunRecomandationBay.java
+++ src/main/java/net/projektfriedhof/ngplus/main/RunRecomandationBay.java
@@ -1,5 +1,11 @@
 package net.projektfriedhof.ngplus.main;
 
+import java.io.File;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.hsqldb.persist.HsqlProperties;
 import org.hsqldb.server.Server;
 import org.slf4j.Logger;
@@ -17,6 +23,29 @@ public class RunRecomandationBay {
         // the initialization phase of your application
        // SLF4JBridgeHandler.install();
 
+    	/*#
+    	 *Commandlione settings: 
+    	 *
+    	 *	-port
+    	 *	-dbdir
+    	 *	-logdir
+    	 *	-da war noch was 
+    	 *	-
+    	 */
+    	
+    	// create Options object
+    	Options options = new Options();
+    	
+    	// add t option
+    	options.addOption("port", true, "Port to listen to. Default: 8080");
+    	options.addOption("logDir", true, "Directory to log to. Default is " + System.getProperty("user.dir"));
+    	options.addOption("database", true, "HSQLDB");
+    	
+    	
+    	CommandLineParser parser = new PosixParser();
+    	CommandLine cmd = parser.parse( options, args);
+    	
+    	
     	startSQLServer();
     	
         try {
